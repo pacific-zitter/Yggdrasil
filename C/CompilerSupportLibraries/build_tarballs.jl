a8a1c2a5b8e1174c688240bc459424901bbef733
Elliot Saba <staticfloat@gmail.com>
2020-01-31 11:49:35 -0800

    Upgrade CompilerSupportLibraries
    We are going to be using the CSL JLL to provide the compiler support
    libraries for the base Julia distribution; to make this happen, we need
    to provide not just matching versions of `libgcc_s` and `libgfortran`
    and whatnot, but also the _latest_ versions of `libstdc++` and
    `libgomp`.  This allows us to link against the widest possible set of
    binaries.

---
+++ C/CompilerSupportLibraries/build_tarballs.jl
@@ -0,0 +1,91 @@
+using BinaryBuilder
+
+name = "CompilerSupportLibraries"
+bb_version = BinaryBuilder.get_bb_version()
+version = VersionNumber(bb_version.major, bb_version.minor, bb_version.patch)
+
+# We are going to need to extract the latest libstdc++ and libgomp from BB
+# So let's grab them into tarballs by using preferred_gcc_version:
+extraction_script = raw"""
+mkdir -p ${libdir}
+for d in /opt/${target}/${target}/lib*; do
+    # Copy all the libstdc++ and libgomp files:
+    cp -av ${d}/libstdc++*.${dlext}* ${libdir} || true
+    cp -av ${d}/libgomp*.${dlext}* ${libdir} || true
+
+    # Don't copy `.a` or `.py` files.  >:[
+    rm -f ${libdir}/*.a
+    rm -f ${libdir}/*.py
+done
+"""
+
+extraction_platforms = supported_platforms()
+extraction_products = [
+    LibraryProduct("libstdc++", :libstdcxx),
+    LibraryProduct("libgomp", :libgomp),
+]
+build_info = autobuild(joinpath(@__DIR__, "build", "extraction"),
+    "LatestLibraries",
+    version,
+    [],
+    extraction_script,
+    extraction_platforms,
+    extraction_products,
+    [];
+    skip_audit=true,
+    preferred_gcc_version=v"100",
+    verbose="--verbose" in ARGS,
+    debug="--debug" in ARGS,
+)
+
+
+
+## Now that we've got those tarballs, we're going to use them as sources to overwrite
+## the libstdc++ and libgomp that we would otherwise get from our compiler shards:
+
+script = raw"""
+mkdir -p ${libdir}
+
+# copy out all the libraries we can find
+for d in /opt/${target}/${target}/lib*; do
+    cp -av ${d}/*.${dlext}* ${libdir} || true
+
+    # Delete .a and .py files
+    rm -f ${libdir}/*.a ${libdir}/*.py
+done
+
+# change permissions so that rpath succeeds
+for l in ${libdir}/*; do
+    chmod 0755 "${l}"
+done
+
+# libgcc_s.1.dylib receives special treatment for now
+if [[ ${target} == *apple* ]]; then
+    install_name_tool -id @rpath/libgcc_s.1.dylib ${libdir}/libgcc_s.1.dylib
+fi
+
+# Install license (we license these all as GPL3, since they're from GCC)
+install_license /usr/share/licenses/GPL3
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = expand_gfortran_versions(supported_platforms())
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct(["libgcc_s", "libgcc_s_sjlj", "libgcc_s_seh"], :libgcc_s),
+    LibraryProduct("libstdc++", :libstdcxx),
+    LibraryProduct("libgfortran", :libgfortran),
+    LibraryProduct("libgomp", :libgomp),
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+for platform in platforms
+    # Find the corresponding source for this platform
+    tarball_path, tarball_hash = build_info[BinaryBuilder.abi_agnostic(platform)][1:2]
+    sources = [
+        tarball_path => tarball_hash,
+    ]
+    build_tarballs(ARGS, name, version, sources, script, [platform], products, [])
+end
